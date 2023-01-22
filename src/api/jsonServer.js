a51c069bf9fc4fb0c9ff666e53b7537d539d8908
Deivid Gomes <davidsilva29@hotmail.com>
2023-01-22 16:08:00 -0300

    Initial commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://9ed4-170-84-225-249.ngrok.io'
+});
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,73 @@
+import React, { useContext, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+const IndexScreen = ({ navigation }) => {
+    const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+
+    console.log(state);
+
+    useEffect(() => {
+        getBlogPosts();
+
+        const listener = navigation.addListener('didFocus', () => {
+            getBlogPosts();
+        });
+
+        return () => {
+            listener.remove();
+        };
+    }, []);
+
+    return <View>
+        <FlatList style={styles.flatList} data={state}
+            keyExtractor={blogPost => blogPost.id}
+            renderItem={({ item }) => {
+                return (<TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                    <View style={styles.row}>
+                        <Text style={styles.title}>{item.title} - {item.id}</Text>
+                        <TouchableOpacity onPress={() => {
+                            deleteBlogPost(item.id);
+                            console.log(item.id);
+                        }}>
+                            <Feather name="trash" style={styles.icon} />
+                        </TouchableOpacity>
+                    </View>
+                </TouchableOpacity>)
+            }}>
+        </FlatList>
+    </View>
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+          <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+            <Feather name="plus" size={30} />
+          </TouchableOpacity>
+        )
+      };
+};
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    },
+    flatList: {
+        marginVertical: 2
+    }
+});
+
+export default IndexScreen;
